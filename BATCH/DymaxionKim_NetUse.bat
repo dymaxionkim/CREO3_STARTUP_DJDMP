7a335fa18def21b125619cd3d9a500bb13fa314c
dymaxionkim <dymaxion.kim@gmail.com>
2017-03-05 13:45:38 +0900

    Net use

--- BATCH/DymaxionKim_NetUse.bat
+++ BATCH/DymaxionKim_NetUse.bat
@@ -2,6 +2,7 @@ title DymaxionKim_NetUse
 REM 2017-03-04
 REM by Dymaxion.Kim@gmail.com
 
+net use /delete *
 @echo on
 
 REM If you connect E: drive on Network-Drive, Please copy this file into c: drive, and run this script as Administrator.
